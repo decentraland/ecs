f3c8c88458cd2a37ee50a4370cceda6caec20fd0
Leandro Mendoza <leandro@decentraland.org>
2022-04-26 15:09:09 -0300

    add protocol buffer dependency and change BoxShape to it
    clean dependencies

--- src/components/legacy/BoxShape/index.ts
+++ src/components/legacy/BoxShape/index.ts
@@ -1,40 +1,16 @@
 import { EcsType } from '../../../built-in-types/EcsType'
-import { BaseShape } from './../Shape'
-import { BoxShape as fbBoxShape } from './box-shape'
-import { Builder, ByteBuffer as FlatBufferByteBuffer } from 'flatbuffers'
 import { ByteBuffer } from '../../../serialization/ByteBuffer'
+import { PBBoxShape } from './BoxShape'
 
-type BoxShape = BaseShape & {
-  uvs: number[]
-}
+export const BoxShape: EcsType<PBBoxShape> = {
+  serialize(value: PBBoxShape, builder: ByteBuffer): void {
+    const writer = PBBoxShape.encode(value)
+    const buffer = new Uint8Array(writer.finish(), 0, writer.len)
 
-export const BoxShape: EcsType<BoxShape> = {
-  serialize(value: BoxShape, builder: ByteBuffer): void {
-    const fbBuilder = new Builder()
-    const boxShape = fbBoxShape.createBoxShape(
-      fbBuilder,
-      value.withCollisions,
-      value.isPointerBlocker,
-      value.visible,
-      fbBoxShape.createUvsVector(fbBuilder, value.uvs)
-    )
-    fbBuilder.finish(boxShape)
-
-    builder.writeBuffer(fbBuilder.asUint8Array(), false)
+    builder.writeBuffer(buffer)
   },
-  deserialize(reader: ByteBuffer): BoxShape {
-    const buf = new FlatBufferByteBuffer(reader.buffer())
-    const boxShape = fbBoxShape.getRootAsBoxShape(buf)
-
-    const newValue: BoxShape = {
-      visible: boxShape.visible(),
-      isPointerBlocker: boxShape.visible(),
-      withCollisions: boxShape.visible(),
-      uvs: Array.from(
-        { length: boxShape.uvsLength() },
-        (_v, k) => boxShape.uvs(k) || 0
-      )
-    }
-    return newValue
+  deserialize(reader: ByteBuffer): PBBoxShape {
+    const buf = reader.readBuffer()
+    return PBBoxShape.decode(buf, buf.length)
   }
 }
